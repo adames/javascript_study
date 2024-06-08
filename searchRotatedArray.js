30197d8a0f5e02eb74eda79e2901226e6a9b780c
adames <adames.hodelin@gmail.com>
2024-06-08 12:07:43 -0400

    newa

--- searchRotatedArray.js
+++ searchRotatedArray.js
@@ -2,7 +2,6 @@
 function search(nums, target) {
     if (nums.length === 0) return -1;
     if (nums.length === 1 && nums[0] === target) return 0;
-
     let left = 0;
     let right = nums.length - 1;
     let mid;
@@ -21,12 +20,11 @@ function search(nums, target) {
             if (target > nums[mid] && target <= nums[right]) {
                 left = mid + 1;
             } else {
-                right = mid - 1
+                right = mid -1
             }
         }
     }
     return -1;
-    
 };
 let nums = [4,5,6,7,0,1,2];
 let target = 0;
