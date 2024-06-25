7c44bb7e9db8b4bab1d27bdb2ecbae23bdd7790c
adames <adames.hodelin@gmail.com>
2024-06-25 08:27:20 -0400

    new

---
+++ gonBeNew.js
@@ -0,0 +1,7 @@
+// Create a method that reverses a sentence, no punctuation
+let sentence = "the quick brown fox jumped over the lazy dog"
+if (process.argv.length > 2) {
+    sentence = process.argv.slice(2).join(" ");
+}
+
+console.log(reverseSentence(sentence));
