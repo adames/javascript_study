e10ce4cadf23c791931ba75a7ac7dd22e974db3a
adames <adames.hodelin@gmail.com>
2024-07-17 16:21:53 -0400

    asdf

--- testingBuySell.js
+++ testingBuySell.js
@@ -3,12 +3,5 @@ let sentence = "the quick brown fox jumped over the lazy dog"
 if (process.argv.length > 2) {
     sentence = process.argv.slice(2).join(" ");
 }
-let reversedSentence= [];
-function reverseSentence(sentence) {
-    let splitSentence = sentence.split(" ");
-    for (let index = splitSentence.length - 1; index >= 0; index--) {
-        reversedSentence.push(splitSentence[index]);
-    }
-    return reversedSentence.join(" ");
-}
+
 console.log(reverseSentence(sentence));
